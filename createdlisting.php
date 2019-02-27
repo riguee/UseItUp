b553babaec1095bc3456f09c3649aedabae896bc
riguee <46566205+riguee@users.noreply.github.com>
2019-02-27 14:50:03 +0000

    message

--- createdlisting.php
+++ createdlisting.php
@@ -30,31 +30,38 @@ $timeuntil = $_POST["until"];
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "test";
+$db = "useitup";
 $conn = new mysqli($servername, $username, $password, $db);
 $recconn = new mysqli($servername, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+//include 'connection.php';
 class Listing{
-    public $listingID, $Title, $Description, $portions, $timefrom, $timeuntil, $allergenname, $diet  ;
+    public $id, $title, $description, $portions, $time_from, $time_until, $allergen, $diet  ;
+    /*function setlisting($id) {
+        include 'connection.php';
+        $query = "SELECT id, title, description, portions, time_from, time_until FROM listings
+WHERE id = ". $id;
+        $result = mysqli_query($conn,$query);
+        $this->Title =
+    }*/
     function display() {?>
         <div class="card">
-            <h5 class="card-header">Listing number: <?php echo $this->listingID; ?></h5>
+            <h5 class="card-header">Listing number: <?php echo $this->id; ?></h5>
             <div class="row">
                 <div class="col-2">
                     <div class="middle">
-                        <p>Pickup window:<br><?php echo $this->timefrom; ?><br><?php echo $this->timeuntil; ?></p>
+                        <p>Pickup window:<br><?php echo $this->time_from; ?><br><?php echo $this->time_until; ?></p>
                     </div>
                 </div>
                 <class="col-10">
-                Dish name: <?php echo $this->Title; ?><br>
-                Dish description: <?php echo $this->Description; ?><br>
+                Dish name: <?php echo $this->title; ?><br>
+                Dish description: <?php echo $this->description; ?><br>
                 Number of portions: <?php echo $this->portions; ?><br>
                 Allergens <ul> <?php
-                    if(isset($this->allergenname)) {
-                        foreach ($this->allergenname as $this_allergen) {
+                    if(isset($this->allergen)) {
+                        foreach ($this->allergen as $this_allergen) {
                             echo "<li>".$this_allergen. "</li>";
                         }
                     }
@@ -79,7 +86,7 @@ class Listing{
 
 }
 
-$createlisting = $conn->prepare("INSERT INTO listings (Title, Description, portions, timefrom, timeuntil, dayposted)
+$createlisting = $conn->prepare("INSERT INTO listings (title, description, portions, time_from, time_until, day_posted)
 VALUES (?, ?, ?, ?, ?, NOW())");
 $createlisting->bind_param("sssss", $title, $description, $portion, $timefrom, $timeuntil);
 $createlisting->execute();
@@ -87,7 +94,7 @@ $createlisting->execute();
 $new_id = mysqli_insert_id($conn);
 if (isset($_POST["allergen"])) {
     foreach ($_POST["allergen"] as $curr_allergen_id) {
-        $setallergens = $conn->prepare("INSERT INTO allergen_litings (listingID, allergenID) VALUES (?, ?)");
+        $setallergens = $conn->prepare("INSERT INTO allergen_listings (listing_id, allergen_id) VALUES (?, ?)");
         $setallergens->bind_param("ii", $new_id, $curr_allergen_id);
         $setallergens->execute();
     }
@@ -95,23 +102,23 @@ if (isset($_POST["allergen"])) {
 
 if (isset($_POST["diet"])) {
     foreach ($_POST["diet"] as $curr_diet_id) {
-        $setdiet = $conn->prepare("INSERT INTO diet_listing (listingID, dietID) VALUES (?, ?)");
+        $setdiet = $conn->prepare("INSERT INTO diet_listings (listing_id, diet_id) VALUES (?, ?)");
         $setdiet->bind_param("ii", $new_id, $curr_diet_id);
         $setdiet->execute();
     }
 }
 
-$query = "SELECT listingID, Title, Description, portions, timefrom, timeuntil FROM listings
-WHERE listingID = ". $new_id;
+$query = "SELECT id, title, description, portions, time_from, time_until FROM listings
+WHERE id = ". $new_id;
 $result = mysqli_query($conn,$query);
 $createdlisting = $result->fetch_object("Listing");
 
 
-$allergenquery = "SELECT allergens.allergenname FROM allergens JOIN allergen_litings ON allergens.allergenID=allergen_litings.allergenID
-JOIN listings ON listings.listingID=allergen_litings.listingID WHERE listings.listingID=".$new_id;
+$allergenquery = "SELECT allergens.allergen FROM allergens JOIN allergen_listings ON allergens.id=allergen_listings.allergen_id
+JOIN listings ON listings.id=allergen_lidtings.listing_id WHERE listings.id=".$new_id;
 
-$stmt = $conn->prepare("SELECT allergens.allergenname FROM allergens JOIN allergen_litings ON allergens.allergenID=allergen_litings.allergenID
-JOIN listings ON listings.listingID=allergen_litings.listingID WHERE listings.listingID=".$new_id);
+$stmt = $conn->prepare("SELECT allergens.allergen FROM allergens JOIN allergen_listings ON allergens.id=allergen_listings.allergen_id
+JOIN listings ON listings.id=allergen_listings.listing_id WHERE listings.id=".$new_id);
 $stmt->execute();
 $allergenresult = $stmt->get_result();
 if (mysqli_num_rows($allergenresult) > 0) {
@@ -119,13 +126,13 @@ if (mysqli_num_rows($allergenresult) > 0) {
     // output data of each row
     while($row = $allergenresult->fetch_row()) {
         array_push($allergenlist,$row[0]);
-        $createdlisting->allergenname = $allergenlist;
+        $createdlisting->allergen = $allergenlist;
     }
 }
 
-$dietstmt = $conn->prepare("SELECT diet_requirements.dietname FROM diet_requirements JOIN diet_listing 
-ON diet_requirements.dietID=diet_listing.dietID
-JOIN listings ON listings.listingID=diet_listing.listingID WHERE listings.listingID=".$new_id);
+$dietstmt = $conn->prepare("SELECT diets.diet FROM diets JOIN diet_listings
+ON diets.id=diet_listings.diet_id
+JOIN listings ON listings.id=diet_listings.listing_id WHERE listings.id=".$new_id);
 $dietstmt->execute();
 $dietresult = $dietstmt->get_result();
 if (mysqli_num_rows($dietresult) > 0) {

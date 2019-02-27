13f67cdff4a6fd1df2155bc13c2f6eb207b03dfe
riguee <46566205+riguee@users.noreply.github.com>
2019-02-27 14:50:03 +0000

    message

--- navbarcharity.php
+++ navbarcharity.php
@@ -2,10 +2,10 @@
 
 <div class="topnav">
     <a class="active" href="index.html">UseItUp</a>
-    <a href="#movies">My account</a>
-    <a href="#recentlyadded">My orders</a>
-    <a href="#mylist">Find listings around me</a>
-    <a href="#account.php">A complaint?</a>
+    <a href="CharityProfileAcct.html">My account</a>
+    <a href="charityorder.html">My orders</a>
+    <a href="mainlisting.php">Find listings around me</a>
+    <a href="Complaint.php">A complaint?</a>
     <div class="search-container">
         <form method="post" action="search.php">
             <input type="text" placeholder="Search.." id="search" name="search">

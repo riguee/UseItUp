c4ec2b27d898a9d023840def256106f7464d6189
riguee <46566205+riguee@users.noreply.github.com>
2019-03-15 15:57:06 +0000

    tiny change

--- new-listing.php
+++ new-listing.php
@@ -102,7 +102,7 @@
         <textarea type="text" class="form-control" rows="5" id="description" name="description" placeholder="e.g. ingredients, flavour, etc."></textarea>
         <br>
         <label for="image">Choose an image</label>
-        <input type="file" id="image" class="form-control-file" placeholder="image" name="image" accept="image/gif, image/jpeg, image/png">
+        <input type="file" id="image" class="form-control-file" placeholder="image" name="fileToUpload" accept="image/gif, image/jpeg, image/png">
         <br>
         <label for="portions">Quantity</label>
         <input type="number" name="portions" id="portions" class="form-control" min="1">

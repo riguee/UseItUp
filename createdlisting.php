86a960475a516179918654d1f3e43e8a939ee4ea
riguee <46566205+riguee@users.noreply.github.com>
2019-03-15 16:11:23 +0000

    tiny change

--- createdlisting.php
+++ createdlisting.php
@@ -31,7 +31,6 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if ($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
@@ -71,7 +70,7 @@ if(isset($_POST["submit"])) {
         }
     }
 
-}
+
 //include 'connection.php';
 include 'Listings.php';
 $restaurant = 1;

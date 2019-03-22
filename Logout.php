6a39a60dd3f062c0277148b60b4f315f0064ee57
riguee <46566205+riguee@users.noreply.github.com>
2019-03-22 14:38:45 +0000

    changed the logout page a bit

--- Logout.php
+++ Logout.php
@@ -7,6 +7,11 @@ session_destroy();
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <title>Logout</title>
 </head>
@@ -15,9 +20,11 @@ session_destroy();
 <div class="form">
     <h1>Thanks for stopping by</h1>
 
-    <p><?= 'You have been logged out!'; ?></p>
+    <p style="text-align: center"><?= 'You have been logged out!'; ?></p>
 
-    <a href="Login.php"><button class="button button-block"/>Home</button></a>
+    <div class="col-6 mx-auto">
+        <a href="Login.php"><button class="btn btn-primary btn-block">Click here to login again</button></a>
+    </div>
 
 </div>
 </body>

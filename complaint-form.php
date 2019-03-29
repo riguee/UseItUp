eb017ee6dd3bfdf0af7e2ecf919b988641345d24
Teck Ruey Lee <zcestrl@ucl.ac.uk>
2019-03-29 18:51:38 +0000

    Added the position of error message when user registers with  similar email

--- complaint-form.php
+++ complaint-form.php
@@ -6,7 +6,7 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
-    <title>Complaint Form</title>
+    <title>Report a Problem</title>
 </head>
 <body>
 <?php
@@ -24,13 +24,13 @@ else {
     header( "location: logout.php" );
 }?>
 <div class="container">
-    <h1>Complaint Form</h1>
-    <p>Please fill in this form to lodge a complain.</p>
+    <h1>Report a Problem</h1>
+    <p>Please fill in this form to lodge a complaint or report a problem regarding your order.</p>
     <hr>
     <form method="post" action="submit-complaint.php">
-        <label for="subject">Subject of complaint</label>
+        <label for="subject">Subject</label>
         <input type="text" class="form-control" placeholder="Enter subject" name="subject" id="subject" required value="<?php if (isset($_POST['order'])) {print("Order #" . $_POST['order']);} ?>">
-        <small class="form-text text-muted">If your complaint concerns a specific order, please write the order's ID as the subject of the complaint.</small>
+        <small class="form-text text-muted">If your issue concerns a specific order, please write the order's ID as the subject.</small>
         <br>
         <label for="complaint">Description</label>
         <textarea name="complaint" id="complaint" class="form-control" rows="4" cols="50" placeholder="<?php if ($_SESSION['user_type'] == 'restaurant') {print("e.g. The charity did not show up or showed up late");} ?> <?php if ($_SESSION['user_type'] == 'charity') {print("e.g. The restaurant had closed or there was a mistake in the order");} ?>"></textarea>
